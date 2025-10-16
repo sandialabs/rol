def9f43c1ba84853eff7f4d7ee8b2dc3e2e794b1
Drew Kouri <dpkouri@sandia.gov>
2025-10-16 08:42:23 -0600

    Updated elasticity example from TRSPG paper.

--- example/PDE-OPT/published/TRSPG_Kouri2021/elasticity/src/tractionK.hpp
+++ example/PDE-OPT/published/TRSPG_Kouri2021/elasticity/src/tractionK.hpp
@@ -12,29 +12,33 @@
            optimization problem.
 */
 
-#ifndef ROL_PDEOPT_ELASTICITY_TRACTION_HPP
-#define ROL_PDEOPT_ELASTICITY_TRACTION_HPP
-
-#include "ROL_Ptr.hpp"
-#include "Teuchos_ParameterList.hpp"
-#include "Intrepid_FieldContainer.hpp"
-
-#include "../../../../TOOLS/fe.hpp"
+#ifndef ROL_PDEOPT_ELASTICITY_TRACTIONK_HPP
+#define ROL_PDEOPT_ELASTICITY_TRACTIONK_HPP
 
 #include <vector>
+#include "ROL_Ptr.hpp"
+#include "ROL_ParameterList.hpp"
+#include "../../../../TOOLS/feK.hpp"
 
-template<class Real>
+template<class Real, class DeviceType>
 class Traction {
+public:
+  using scalar_view = Kokkos::DynRankView<Real, DeviceType>;
+  using basis_ptr   = Intrepid2::BasisPtr<DeviceType, Real, Real>;
+  using fe_type     = FE<Real,DeviceType>;
+  using fst         = Intrepid2::FunctionSpaceTools<DeviceType>;
+  using ct          = Intrepid2::CellTools<DeviceType>;
+  using rst         = Intrepid2::RealSpaceTools<DeviceType>;
+
 private:
   std::vector<int> sidesets_, sideids_;
-  std::vector<Real> loadMagnitude_;
-  std::vector<Real> loadPolar_, loadAzimuth_;
-  std::vector<std::vector<ROL::Ptr<Intrepid::FieldContainer<Real>>>> bdryCellNodes_;
+  std::vector<Real> loadMagnitude_, loadPolar_, loadAzimuth_;
+  std::vector<std::vector<scalar_view>> bdryCellNodes_;
   std::vector<std::vector<std::vector<int>>> bdryCellLocIds_;
   int dim_, offset_;
 
 protected:
-  Real DegreesToRadians(const Real deg) const {
+  Real DegreesToRadians(Real deg) const {
     return deg * static_cast<Real>(M_PI) / static_cast<Real>(180);
   }
 
@@ -75,60 +79,48 @@ public:
     return (sidesets_.size()==0);
   }
 
-  void setCellNodes(const std::vector<std::vector<ROL::Ptr<Intrepid::FieldContainer<Real>>>> &bdryCellNodes,
+  void setCellNodes(const std::vector<std::vector<scalar_view>> &bdryCellNodes,
                     const std::vector<std::vector<std::vector<int>>> &bdryCellLocIds) {
     bdryCellNodes_  = bdryCellNodes;
     bdryCellLocIds_ = bdryCellLocIds;
   }
 
-  virtual Real evaluate(const int                dir,
-                        const int                sideset,
-                        const std::vector<Real> &param) const {
-//    const int numSideSets = sidesets_.size();
-    const int paramSize   = param.size();
-    Real val(0);
-    Real loadMagNoise(0), loadAngNoise0(0);
-    if (paramSize > 0 + dim_*sideset) {
+  virtual Real evaluate(int dir, int sideset, const std::vector<Real> &param) const {
+    const int paramSize = param.size();
+    Real val(0), loadMagNoise(0), loadAngNoise0(0);
+    if (paramSize > 0 + dim_*sideset)
       loadMagNoise  = param[offset_ + 0 + dim_*sideset];
-    }
-    if (paramSize > 1 + dim_*sideset) {
+    if (paramSize > 1 + dim_*sideset)
       loadAngNoise0 = DegreesToRadians(param[offset_ + 1 + dim_*sideset]);
-    }
     const Real loadMagnitude = loadMagnitude_[sideset] + loadMagNoise;
     const Real loadAngle0    = loadPolar_[sideset]     + loadAngNoise0;
 
     if (dim_==2) {
-      if (dir==0) {
+      if (dir==0)
         val = loadMagnitude*std::cos(loadAngle0);
-      }
-      if (dir==1) {
+      if (dir==1)
         val = loadMagnitude*std::sin(loadAngle0);
-      }
     }
     if (dim_==3) {
       Real loadAngNoise1(0);
-      if (paramSize > 2 + dim_*sideset) {
+      if (paramSize > 2 + dim_*sideset)
         loadAngNoise1 = DegreesToRadians(param[offset_ + 2 + dim_*sideset]);
-      }
       const Real loadAngle1 = loadAzimuth_[sideset] + loadAngNoise1;
 
-      if (dir==0) {
+      if (dir==0)
         val = loadMagnitude*std::sin(loadAngle0)*std::cos(loadAngle1);
-      }
-      if (dir==1) {
+      if (dir==1)
         val = loadMagnitude*std::sin(loadAngle0)*std::sin(loadAngle1);
-      }
-      if (dir==2) {
+      if (dir==2)
         val = loadMagnitude*std::cos(loadAngle0);
-      }
     }
     return val;
   }
 
-  void compute(std::vector<std::vector<std::vector<ROL::Ptr<Intrepid::FieldContainer<Real>>>>> &traction,
-               const std::vector<std::vector<ROL::Ptr<FE<Real>>>>                              &fe,
-               const std::vector<Real>                                                         &param,
-               const Real                                                                       scale = Real(1)) const {
+  void compute(std::vector<std::vector<std::vector<scalar_view>>> &traction,
+               const std::vector<std::vector<ROL::Ptr<fe_type>>> &fe,
+               const std::vector<Real> &param,
+               Real scale = Real(1)) const {
     traction.clear();
     traction.resize(bdryCellLocIds_.size());
     const int numSideSets = sidesets_.size();
@@ -139,15 +131,13 @@ public:
         for (int j = 0; j < numLocalSideIds; ++j) {
           const int numCellsSide = bdryCellLocIds_[sidesets_[i]][j].size();
           if (numCellsSide > 0) {
-            const int numCubPerSide = fe[sidesets_[i]][j]->cubPts()->dimension(1);
+            const int numCubPerSide = fe[sidesets_[i]][j]->cubPts().extent_int(1);
             traction[sidesets_[i]][j].resize(dim_);
             for (int k = 0; k < dim_; ++k) {
-              traction[sidesets_[i]][j][k]
-                = ROL::makePtr<Intrepid::FieldContainer<Real>>(numCellsSide, numCubPerSide);
+              traction[sidesets_[i]][j][k] = scalar_view("traction", numCellsSide, numCubPerSide);
               for (int c = 0; c < numCellsSide; ++c) {
-                for (int p = 0; p < numCubPerSide; ++p) {
-                  (*traction[sidesets_[i]][j][k])(c,p) = scale*evaluate(k,i,param);
-                }
+                for (int p = 0; p < numCubPerSide; ++p)
+                  (traction[sidesets_[i]][j][k])(c,p) = scale*evaluate(k,i,param);
               }
             }
           }
@@ -156,36 +146,32 @@ public:
     }
   }
 
-  void apply(std::vector<ROL::Ptr<Intrepid::FieldContainer<Real>>> &R,
-             const std::vector<std::vector<ROL::Ptr<FE<Real>>>>    &fe,
-             const std::vector<Real>                               &param,
-             const Real                                             scale = Real(1)) const {
+  void apply(std::vector<scalar_view> &R,
+             const std::vector<std::vector<ROL::Ptr<fe_type>>> &fe,
+             const std::vector<Real> &param,
+             Real scale = Real(1)) const {
     const int numSideSets = sidesets_.size();
-    const int nf          = R[0]->dimension(1);
+    const int nf          = R[0].extent_int(1);
     if (numSideSets > 0) {
       for (int i = 0; i < numSideSets; ++i) {
         const int numLocalSideIds = bdryCellLocIds_[sidesets_[i]].size();
         for (int j = 0; j < numLocalSideIds; ++j) {
           const int numCellsSide = bdryCellLocIds_[sidesets_[i]][j].size();
           if (numCellsSide > 0) {
-            const int numCubPerSide = fe[sidesets_[i]][j]->cubPts()->dimension(1);
+            const int numCubPerSide = fe[sidesets_[i]][j]->cubPts().extent_int(1);
             for (int k = 0; k < dim_; ++k) {
-              Intrepid::FieldContainer<Real> traction(numCellsSide, numCubPerSide);
+              scalar_view traction("traction", numCellsSide, numCubPerSide);
               for (int c = 0; c < numCellsSide; ++c) {
-                for (int p = 0; p < numCubPerSide; ++p) {
+                for (int p = 0; p < numCubPerSide; ++p)
                   traction(c,p) = scale*evaluate(k,i,param);
-                }
               }
-              Intrepid::FieldContainer<Real> tractionResidual(numCellsSide, nf);
-              Intrepid::FunctionSpaceTools::integrate<Real>(tractionResidual,
-                                                            traction,
-                                                            *fe[sidesets_[i]][j]->NdetJ(),
-                                                            Intrepid::COMP_CPP, false);
+              scalar_view tractionResidual("tractionResidual", numCellsSide, nf);
+              fst::integrate(tractionResidual, traction, fe[sidesets_[i]][j]->NdetJ(), false);
               // Add traction residual to volume residual
               for (int l = 0; l < numCellsSide; ++l) {
                 int cidx = bdryCellLocIds_[sidesets_[i]][j][l];
                 for (int m = 0; m < nf; ++m) {
-                  (*R[k])(cidx,m) += tractionResidual(l,m);
+                  (R[k])(cidx,m) += tractionResidual(l,m);
                 }
               }
             }
